a6e33a90606dbe266d984724eca6096a32eb583b
OmarBELK <omar.belkasseh@gmail.com>
2024-12-27 21:46:28 +0100

    change font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontSize: {
-        'xs': '0.875rem',     // 14px
-        'sm': '1rem',         // 16px
-        'base': '1.125rem',   // 18px
-        'lg': '1.25rem',      // 20px
-        'xl': '1.375rem',     // 22px
-        '2xl': '1.5rem',      // 24px
-        '3xl': '1.875rem',    // 30px
-        '4xl': '2.25rem',     // 36px
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
